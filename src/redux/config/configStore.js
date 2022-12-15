4ac5dc8da130329f4a9d5d4b529cca41d773c9a7
ptg0811 <ptg0811@gmail.com>
2022-12-15 20:38:26 +0900

    진짜_최종

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import memeReducer from "../modules/homeMod";
 import contents from "../modules/detailMod";
-import postComment from "../modules/postCommentForm";
 
 const store = configureStore({
   reducer: {
