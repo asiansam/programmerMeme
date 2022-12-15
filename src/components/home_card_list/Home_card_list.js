55865b15a45cb8b3ec5f82fa407b44bf27fb4d9f
exit-Han <han@gdp.works>
2022-12-15 17:37:15 +0900

    db title 보여주려다. 한 카드에 title 8개 중복 생성되서 HomeCards.js 컴포넌트 생성 후 빼뒀음

--- src/components/home_card_list/Home_card_list.js
+++ src/components/home_card_list/Home_card_list.js
@@ -1,8 +1,79 @@
-import React from "react";
 import styled from "styled-components";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { __getCardsList, __delcardsList } from "../../redux/modules/memesCardsSlice";
+import HomeCards from "./HomeCards";
+import axios from "axios";
 
-const Home_card_list = () => {
-  return <div>카드리스트입니다.</div>;
+const HomeCardList = () => {
+  // const dispatch = useDispatch();
+
+  // const { isLoading, error, cards } = useSelector((state) => state.cards);
+
+  // useEffect(() => {
+  //   dispatch(__getCardsList())
+  // }, [dispatch])
+
+  // console.log('cards', cards)
+
+  // if (isLoading) {
+  //   return <div>로딩 중....</div>
+  // }
+
+  // if (error) {
+  //   return <div>{error.message}</div>;
+  // }
+  // data.data.title
+
+  return (
+
+    <div>
+      <HomeCards />
+    </div>
+    // <Main_Home>
+    //   <Main_Home_Card_Set>
+    //     <Home_Cards_Lsit_Btn_Box>
+    //       <Btn>
+    //         수정
+    //       </Btn>
+    //       <Btn>
+    //         삭제
+    //       </Btn>
+    //     </Home_Cards_Lsit_Btn_Box>
+    //     <Home_Cards_Lsit_Img>
+    //       <div>{cards.map((card) => (
+    //         <image key={card.id} source={card.url} />
+    //       ))}</div>
+
+    //     </Home_Cards_Lsit_Img>
+    //     <Desc>
+    //       <Home_Cards_Lsit_title>
+    //         {cards.map((card) => (
+    //           <div key={card.id}>{card.title}</div>
+    //         ))}
+    //       </Home_Cards_Lsit_title>
+    //     </Desc>
+    //   </Main_Home_Card_Set>
+    // </Main_Home>
+  )
+
 };
 
-export default Home_card_list;
+
+export default HomeCardList;
+
+
+//카드 리스트를 맵으로 보여준다.
+//
+
+
+//수정 
+//삭제 
+//리스트
+//타이틀  
+{/* <div>
+  <div>수정</div>
+  <div>삭제</div>
+  <div>이미지</div>
+  <div>타이틀 </div>
+</div> */}
