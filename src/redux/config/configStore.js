e58501b55f22695f6ccfc8b36e7551ddec6a0698
ptg0811 <ptg0811@gmail.com>
2022-12-15 12:31:56 +0900

    action creator 이름 최신화

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -4,7 +4,7 @@ import { configureStore } from "@reduxjs/toolkit";
  * import 해온 것은 slice.reducer 입니다.
  */
 import contents from "../modules/detailMod";
-import { postComment } from "redux/modules/postCommentForm";
+import postComment from "../modules/postCommentForm";
 
 const store = configureStore({
   reducer: { contents: contents, postComment: postComment },
